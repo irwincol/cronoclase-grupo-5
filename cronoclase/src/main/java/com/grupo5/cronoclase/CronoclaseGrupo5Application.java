196ff2d74185d66f379833ac7163677ac7b754e2
irwincol <irwincolmenarez10@gmail.com>
2026-02-18 19:55:49 -0500

    primera subida

---
+++ cronoclase/src/main/java/com/grupo5/cronoclase/CronoclaseGrupo5Application.java
@@ -0,0 +1,13 @@
+package com.grupo5.cronoclase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CronoclaseGrupo5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CronoclaseGrupo5Application.class, args);
+	}
+
+}
